f4fe5b21e9b2c69e92e8e099a2b5b3183efaae09
zhanchao.wu <zhanchao.wu@icloud.com>
2022-03-28 14:53:02 +0800

    feat(基础): 微应用
    less module,git-commit,antd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { join } from 'path';
 
+function resolve(dir: string) {
+  return join(__dirname, dir);
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  css: {
+    modules: {
+      // css模块化 文件以.module.[css|less|scss]结尾
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      hashPrefix: 'prefix',
+    },
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@/': resolve('src'),
+    },
+  },
+  server: {
+    port: 8099,
+    open: true,
+    proxy: {
+      // 字符串简写方式
+      '/admin-web': 'http://10.199.209.111:8088/admin-web',
+    },
+  },
+});
