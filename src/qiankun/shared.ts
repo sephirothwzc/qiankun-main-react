de73fff5ca294b0a23aa523d35589a294c2d8582
zhanchao.wu <zhanchao.wu@icloud.com>
2022-03-30 16:14:52 +0800

    feat(redux): redux
    immer

---
+++ src/qiankun/shared.ts
@@ -0,0 +1,19 @@
+import actions, { LOGIN_ACTION } from '@/redux/action/app-user';
+import store from '@/redux/store';
+import { Maybe } from '@/utils/type-helper';
+
+/**
+ * 获取 Token
+ */
+const getToken = (): Maybe<string> => {
+  const state = store.getState();
+  return state.login.appUser?.token;
+};
+
+/**
+ * 设置 Token
+ */
+const setToken = (token: string): void => {
+  // 将 token 的值记录在 store 中
+  actions[LOGIN_ACTION.LOGIN]({ token });
+};
